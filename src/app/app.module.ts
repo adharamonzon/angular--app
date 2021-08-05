822e5dad1344d3ecefb2471e1adbba51a7e460fd
Adhara Monzon Varillas <amonzonv@everis.com>
2021-08-05 11:49:35 +0200

    add button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GamesListComponent } from './games-list/games-list.component';
 import { GameItemComponent } from './game-item/game-item.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GamesListComponent,
-    GameItemComponent
+    GameItemComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
